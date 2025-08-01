9adf3a27868503f920f7861fcc2e9414c5c65739
Qianyu Huang <qianyuhuang0722@gmail.com>
2025-08-01 17:10:59 -0400

    del ddpg

--- agents/DDPG_Agent.py
+++ agents/DDPG_Agent.py
@@ -11,6 +11,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.ReplayBuffer import ReplayBuffer as ReplayBuffer
 from utils.OUActionNoise import OUActionNoise as OUActionNoise
+from agents.ActorNetwork import ActorNetwork as ActorNetwork
+from agents.CriticNetwork import CrticNetwork as CriticNetwork
 
 class Agent(object):
     def __init__(self, alpha, beta, input_dims, tau, env, gamma, n_actions=2,
@@ -19,3 +21,19 @@ class Agent(object):
         self.gamma = gamma
         self.tau = tau
         self.memory = ReplayBuffer(max_size, input_dims, n_actions)
+        self.batch_size = batch_size
+
+        self.actor = ActorNetwork(alpha=alpha, input_dims=input_dims, fc1_dims=layer1_size, 
+                                fc2_dims=layer2_size, n_actions=n_actions, name='Actor')
+        self.target_actor = ActorNetwork(alpha=alpha, input_dims=input_dims, fc1_dims=layer1_size, 
+                                fc2_dims=layer2_size, n_actions=n_actions, name='TargetActor')
+        
+        self.critic = CriticNetwork(beta=beta, input_dims=input_dims, fc1_dims=layer1_size, 
+                                    fc2_dims=layer2_size, n_actions=n_actions, name='Critic')
+        self.target_critic = CriticNetwork(beta=beta, input_dims=input_dims, fc1_dims=layer1_size, 
+                                    fc2_dims=layer2_size, n_actions=n_actions, name='TargetCritic')
+        
+        self.noise = OUActionNoise(mu=np.zeros(n_actions))
+        self.update_network_parameters(tau=1)
+
+    
--- ddpg.py
+++
@@ -1,9 +0,0 @@
-# source: https://spinningup.openai.com/en/latest/algorithms/ddpg.html, https://arxiv.org/pdf/1509.02971
-# need replay buffer class
-# need tagret q net 
-# batch norm
-# deterministic policy, handle explore exploit with mean-zero Gaussian noise
-# target for actor and crtitic + target for each 
-# soft updates according to theta_prime = tau*theta + (1-tau)*theta_prime, with tau << 1 to stabalize learning :) 
-
-
